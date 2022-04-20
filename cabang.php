4f8489ad952f532ea9f9e33ac5dee113a1a0057a
FadlySatriadi <91892103+FadlySatriadi@users.noreply.github.com>
2022-04-20 17:09:19 +0700

    Add files via upload

---
+++ cabang.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+    </head>
+    <body>
+        <?php
+            $nama = "Fadly Ulya Satriadi";
+
+            if($nama=="Fadly Ulya Satriadi") {
+                echo "Selamat belajar $nama";
+            }
+        ?>
+    </body>
+</html>
